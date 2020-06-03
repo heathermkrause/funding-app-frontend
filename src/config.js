85e6671d8b2a2e8e8ab253e3a5b4ef90ad640e9f
brendeschuijmert <brendeschuijmert@gmail.com>
2020-06-03 15:20:28 -0500

    fixed bug

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
-console.log(process.env)
-
 export const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://localhost:4000'
-    : process.env.REACT_APP_API_URL;
+    ? process.env.process.env.REACT_APP_PROD_API_URL
+    : process.env.process.env.REACT_APP_DEV_API_URL;
 
 export const domain = 'restaurant.review';
 export const PAGE_LIMIT = 4;
